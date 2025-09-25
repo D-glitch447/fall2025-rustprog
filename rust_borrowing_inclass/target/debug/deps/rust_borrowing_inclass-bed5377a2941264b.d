fbc27a649a7b08a1486331c1b9a5e656deaec767
Dana <danasof0305@gmail.com>
2025-09-25 04:16:33 +0000

    Rust Borrowing In-Class Assignment Done

---
+++ rust_borrowing_inclass/target/debug/deps/rust_borrowing_inclass-bed5377a2941264b.d
@@ -0,0 +1,5 @@
+/workspaces/fall2025-rustprog/rust_borrowing_inclass/target/debug/deps/rust_borrowing_inclass-bed5377a2941264b.d: src/main.rs
+
+/workspaces/fall2025-rustprog/rust_borrowing_inclass/target/debug/deps/rust_borrowing_inclass-bed5377a2941264b: src/main.rs
+
+src/main.rs:
---
+++ rust_borrowing_inclass/target/debug/rust_borrowing_inclass.d
@@ -0,0 +1,1 @@
+/workspaces/fall2025-rustprog/rust_borrowing_inclass/target/debug/rust_borrowing_inclass: /workspaces/fall2025-rustprog/rust_borrowing_inclass/src/main.rs
